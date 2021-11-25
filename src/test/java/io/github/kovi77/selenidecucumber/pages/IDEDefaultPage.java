fa8104461723eba9fe93247c4d992d8d92c4ce21
rkeeves <aprettydecentaddress@gmail.com>
2021-11-25 23:56:20 +0100

    fix: removed original example code after copy
    All code has been moved under examples

--- src/test/java/io/github/kovi77/selenidecucumber/pages/IDEDefaultPage.java
+++
@@ -1,9 +0,0 @@
-package io.github.kovi77.selenidecucumber.pages;
-
-import com.codeborne.selenide.SelenideElement;
-
-import static com.codeborne.selenide.Selenide.$;
-
-public class IDEDefaultPage {
-    public SelenideElement title = $(".menu-second-title-box__title");
-}
